5a36b043522e634c4068d38214caf84717efdc79
LeepDev <hyungy@gmail.com>
2023-12-22 15:53:38 -0600

    added more styling and fixed the nav bar for mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,10 +2,11 @@ import { Link, useLocation } from 'react-router-dom'
 import * as userService from '../../utilities/users-service'
 import { useMediaQuery } from 'react-responsive';
 import { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 export default function NavBar ({setUser}) {
     const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
-    const [menuOpen, setMenuOpen] = useState(isMobile);
+    const [menuOpen, setMenuOpen] = useState(false);
     const location = useLocation()
     const nonActiveClass = "block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
     const activeClass = "block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
@@ -21,28 +22,45 @@ export default function NavBar ({setUser}) {
         setUser(null);
     }
 
+    function toggleMenu() {
+        setMenuOpen(!menuOpen)
+    }
+
     return (
         <>
-            <div className="fixed top-0 w-screen h-16 px-6 bg-midnight-blue justify-start items-center gap-8 inline-flex">
+            <div className="fixed top-0 w-screen h-16 px-6 bg-midnight-blue justify-between items-center gap-8 inline-flex">
+                <button id='hamburger' onClick={toggleMenu} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                    <span className="sr-only">Open main menu</span>
+                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+                    </svg>
+                </button>
+                
                 <div className="w-40 h-8 relative block md:none">
                     <Link to="/">
                         <img src="https://assets-global.website-files.com/5d5c50dcee289053140e5d5f/5d5d88ca3e9e5acdd003f839_logo.svg" alt="TradingBlock" />
                     </Link>
                 </div>
-                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-                    </svg>
-                </button>
-                <div id="navbar-default" className="grow shrink basis-0 h-6 justify-start items-center gap-8 flex sm:none">
+                <div id="navbar-default" className="grow shrink basis-0 h-6 justify-start items-center gap-8 lg:flex md:flex hidden">
                     <Link to="/Challenge1" className={location.pathname == "/Challenge1" ? navTextActive : navText}>Challenge 1</Link>
                     <Link to="/Challenge2" className={location.pathname == "/Challenge2" ? navTextActive : navText}>Challenge 2</Link>
                     <Link to="/Challenge2Bonus" className={location.pathname == "/Challenge2Bonus" ? navTextActive : navText}>Challenge 2 (BONUS)</Link>
                     <Link to="/Challenge3" className={location.pathname == "/Challenge3" ? navTextActive : navText}>Challenge 3</Link>
                 </div>
-                <button className="text-white bg-teal hover:bg-teal/70 focus:outline-none focus:ring-4 focus:ring-teal font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2" onClick={handleLogOut}>LOG OUT</button>
+                <button className="min-w-fit text-white bg-teal hover:bg-teal/70 focus:outline-none focus:ring-4 focus:ring-teal font-medium rounded-full text-sm px-5 py-2.5 text-center" onClick={handleLogOut}>LOG OUT</button>
             </div>
+            {
+                menuOpen &&
+                <CSSTransition in={menuOpen} timeout={5000} classNames="fade" unmountOnExit>
+                    <div className="flex bg-gradient-to-b from-midnight-blue to-rgb(255,196,126) top-16 left-0 fixed h-screen w-full flex-col p-10">
+                        <hr className='bg-white mt-[-40px]' />
+                        <Link to="/Challenge1" className={location.pathname == "/Challenge1" ? navTextActive : navText}>Challenge 1</Link>
+                        <Link to="/Challenge2" className={location.pathname == "/Challenge2" ? navTextActive : navText}>Challenge 2</Link>
+                        <Link to="/Challenge2Bonus" className={location.pathname == "/Challenge2Bonus" ? navTextActive : navText}>Challenge 2 (BONUS)</Link>
+                        <Link to="/Challenge3" className={location.pathname == "/Challenge3" ? navTextActive : navText}>Challenge 3</Link>
+                    </div>
+                </CSSTransition>
+            }
         </>
     )
 }
--- src/pages/Challange3/Challenge3.jsx
+++ src/pages/Challange3/Challenge3.jsx
@@ -4,31 +4,84 @@ import { getCurrentQuote } from '../../utilities/fmp-service'
 export default function Challenge3() {
     const [timer, setTimer] = useState(null)
     const [quotes, setQuotes] = useState({})
-
+    const [prevQuotes, setPrevQuotes] = useState({})
+    const [prices, setPrices] = useState({})
+    const [upDown, setUpDown] = useState({})
     const symbolList = ['MS', 'AAPL', 'AMZN']
     
     const updateTimer = () => {
         const randomTime = Math.random() * 3000 + 1000;
 
-        // Update the stocks
         symbolList.forEach(element => {
-            handleStockCall(quotes, element)
+            if (quotes[element])
+            {
+                const randomChange = Math.floor(Math.random() * 3) + 1
+                console.log(randomChange)
+                switch (randomChange) {
+                    case 1:
+                        let price = quotes[element].price
+                        const randomPriceChange = Math.random() * 1 - .5
+                        setQuotes(quotes => ({...quotes, [element]: {...quotes[element], 'price': (price + randomPriceChange)}}))
+                        break
+                    case 2:
+                        let change = quotes[element].change
+                        const randomchange = Math.random() * 1 - .5
+                        setQuotes(quotes => ({...quotes, [element]: {...quotes[element], 'change': (change + randomchange)}}))
+                        break
+                    case 2:
+                        let changesPercentage = quotes[element].changesPercentage
+                        const randomchangesPercentage = Math.random() * 1 - .5
+                        setQuotes(quotes => ({...quotes, [element]: {...quotes[element], 'changesPercentage': (changesPercentage + randomchangesPercentage)}}))
+                        break
+                    default:
+                        break
+                }
+
+            }
         });
-        
+
         // Set a new timeout for the next update
         setTimer(setTimeout(updateTimer, randomTime))
     };
-    
+
     useEffect(() => {
-        // console.log(quotes)
+        if (Object.keys(quotes).length > 0) {
+            symbolList.forEach(element => {
+                if (quotes[element])
+                {
+                    if(!prices[element]){
+                        console.log("priceSet")
+                        setPrices(prices => ({...prices, [element]: quotes[element].price}))
+                    }
+                    else if (quotes[element].price > prices[element]) {
+                        console.log("up")
+                        setUpDown(upDown => ({...upDown, [element]: "up"}))
+                        setPrices(prices => ({...prices, [element]: quotes[element].price}))
+                    } else if (quotes[element].price < prices[element]) {
+                        console.log("down")
+                        setUpDown(upDown => ({...upDown, [element]: "down"}))
+                        setPrices(prices => ({...prices, [element]: quotes[element].price}))
+                    } else if (quotes[element].price === prices[element]) {
+                        console.log("equal")
+                        setUpDown(upDown => ({...upDown, [element]: "equal"}))
+                        setPrices(prices => ({...prices, [element]: quotes[element].price}))
+                    }
+                }
+            })
+        }
     }, [quotes])
     
-    const handleStockCall = async (quotes, element) => {
-        let res
-        
-        res = await getCurrentQuote(element)
+    const setUpStocks = () => {
+        // Update the stocks
+        symbolList.forEach(element => {
+            handleStockCall(element)
+        });
+    }
+    
+    
+    async function handleStockCall(element) {
+        let res = await getCurrentQuote(element)
         setQuotes(quotes => ({...quotes, [element]: res}))
-        return res
     }
 
     const toggleCalls = () => {
@@ -46,7 +99,7 @@ export default function Challenge3() {
         if (price[0] === '-') {
             price = '-$' + price.slice(1)
         } else
-            price += '$'
+            price = '$' + price
         return price
     }
 
@@ -60,7 +113,7 @@ export default function Challenge3() {
 
     return (
     <div className="bg-gradient-to-b from-midnight-blue to-rgb(255,196,126) h-screen w-full flex-ctr-ctr flex-col">
-        <div style={{width: '100vh', padding: '10vh'}} className="flex-ctr-ctr flex-col shadow-teal shadow-2xl rounded-xl bg-midnight-blue  border border-teal/10">
+        <div style={{width: '110vh', padding: '10vh'}} className="flex-ctr-ctr flex-col shadow-teal shadow-2xl rounded-xl bg-midnight-blue  border border-teal/10">
             <div className="relative overflow-x-auto">
                 <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead className="text-xs border-b dark:bg-midnight-blue dark:border-gray-700 dark:text-gray-600">
@@ -98,14 +151,14 @@ export default function Challenge3() {
                                     <th scope="row" className="px-6 py-4 whitespace-nowrap dark:text-white">
                                         {quote.symbol}
                                     </th>
-                                    <td className="px-6 py-4">
-                                        ${quote.price}
+                                    <td className={upDown[element] ? (upDown[element] === "up" ? "px-6 py-4 text-teal transition-colors duration-500 ease-in-out animate__animated animate__pulse" : (upDown[element] === "down" ? "px-6 py-4 text-red-600 transition-colors duration-500 ease-in-out animate__animated animate__pulse" : "px-6 py-4")) : "px-6 py-4"}>
+                                        ${quote.price.toFixed(2)}
                                     </td>
-                                    <td className={isNegative(quote.change) ? "px-6 py-4 text-red-600" : "px-6 py-4 text-teal"}>
-                                        {getChange(quote.change)}
+                                    <td className={isNegative(quote.change.toFixed(2)) ? "px-6 py-4 text-red-600 animate__animated animate__pulse" : "px-6 py-4 text-teal animate__animated animate__pulse"}>
+                                        {getChange(quote.change.toFixed(2))}
                                     </td>
-                                    <td className={isNegative(quote.change) ? "px-6 py-4 text-red-600" : "px-6 py-4 text-teal"}>
-                                        {quote.changesPercentage}%
+                                    <td className={isNegative(quote.change.toFixed(2)) ? "px-6 py-4 text-red-600 animate__animated animate__pulse" : "px-6 py-4 text-teal animate__animated animate__pulse"}>
+                                        {quote.changesPercentage.toFixed(2)}%
                                     </td>
                                     <td className="px-6 py-4">
                                         ${quote.open}
@@ -127,6 +180,7 @@ export default function Challenge3() {
             </div>
         </div>
 
+        <button className='mt-10 text-white bg-teal hover:bg-teal/70 focus:outline-none focus:ring-4 focus:ring-teal font-medium rounded-full text-lg px-5 py-2.5 text-center me-2 mb-2' onClick={setUpStocks}>Set Stocks</button>
         <button className='mt-10 text-white bg-teal hover:bg-teal/70 focus:outline-none focus:ring-4 focus:ring-teal font-medium rounded-full text-lg px-5 py-2.5 text-center me-2 mb-2' onClick={toggleCalls}>{timer ? "Stop Calls" : "Start Calls"}</button>
     </div>
     );
